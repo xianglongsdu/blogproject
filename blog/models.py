3b7b9a7a3dddd3fe59bed5a15a7a723055a5f74b
xianglongsdu <xianglongsdu@163.com>
2018-02-24 18:14:16 +0800

    创建 Django 博客的数据库模型-20180224

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
