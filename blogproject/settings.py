5f6cf868e51ea14947fcb022d276ea9952f438df
xianglongsdu <xianglongsdu@163.com>
2018-03-05 18:11:55 +0800

    后台数据库改为Postgresql

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'blog',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'juedi',
+        'PASSWORD': 'juedi'
     }
 }
 
