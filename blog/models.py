1c9e99d763e099b9368b49db9e77bb36cf1d31f0
xianglongsdu <xianglongsdu@163.com>
2018-03-10 15:39:57 +0800

    真正的 Django 博客首页视图-20180310

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,14 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
@@ -19,6 +25,9 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 
