95b1d8bc0d3dd428e700e0932ea0812c3dd5ae1d
xianglongsdu <xianglongsdu@163.com>
2018-03-11 17:03:43 +0800

    博客文章详情页-20180311

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -29,5 +32,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
-
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk': self.pk})
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path(r'', views.index)
+    path('', views.index),
+    re_path('^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    
 ]
