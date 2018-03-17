606cfb02b0ea31ccf6190799989ab90303ef0b8e
xianglongsdu <xianglongsdu@163.com>
2018-03-17 10:29:04 +0800

    支持 Markdown 语法和代码高亮-20180317

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+import markdown
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
@@ -8,6 +9,7 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body, extensions={'markdown.extensions.extra','markdown.extensions.codehilite', 'markdown.extensions.toc'})
     return render(request, 'blog/detail.html', context={'post': post})
 
 
