a53281e4efbf30eb716adbb66f5df8847f867145
xianglongsdu <xianglongsdu@163.com>
2018-03-18 09:09:42 +0800

    分类与归档-20180318

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     re_path('^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    re_path('^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
+    re_path('^category/(?P<pk>[0-9]+)/$', views.category, name='category')
     
 ]
