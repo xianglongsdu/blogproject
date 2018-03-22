227157759319d122d14a1149c81e32762f233d02
xianglongsdu <xianglongsdu@163.com>
2018-03-22 10:27:49 +0800

    博客文章详情页-20180322

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Category(models.Model):
@@ -28,6 +29,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk': self.pk})
+
 
 
 
