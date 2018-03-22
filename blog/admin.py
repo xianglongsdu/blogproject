e0e7e95e8990e157c9089af46723588894e0e762
xianglongsdu <xianglongsdu@163.com>
2018-03-22 10:27:49 +0800

    博客文章详情页-20180322

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Post, Category, Tag
 
 class PostAdmin(admin.ModelAdmin):
-    fieldsets = (('基本项目', {'fields':('title', 'excerpt', 'body')}), ('高级项目', {'classes': ('collapse',), 'fields': ('category', 'created_time')}))
     list_display = ['title', 'author', 'category', 'created_time']
 
 admin.site.register(Post, PostAdmin)
