e89c7f22ff87badbe93537c3a17d49a4065c7417
xianglongsdu <xianglongsdu@163.com>
2018-03-29 18:34:13 +0800

    分类与归档-20180329

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    re_path(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail')
+    re_path(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
+    re_path(r'category/(?P<pk>[0-9]+)/$', views.category, name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,11 @@ def detail(request, pk):
     post.body = markdown.markdown(post.body, extensions=['markdown.extensions.extra', 'markdown.extensions.codehilite', 'markdown.extensions.toc'])
     
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    post_list = Post.objects.filter(category_id=pk)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
