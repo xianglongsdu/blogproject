8fe30aa678d77353aee2c5218ad0a9ff77975331
xianglongsdu <xianglongsdu@163.com>
2018-06-09 09:32:32 +0800

    admin配置完成-20180609

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'excerpt', 'content','author', 'time'),
+        }),
+        ('高级选项', {
+            'classes': ('collapse',),
+            'fields': ('category', 'tags')
+        })
+    )
+    list_display = ('title', 'excerpt', 'author', 'time')
+    list_display_links = ('title', 'excerpt')
+    list_editable = ('author', 'time')
+    list_filter = ('category', 'tags')
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200, default='')
     content = models.TextField()
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
 
     time = models.DateTimeField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 
