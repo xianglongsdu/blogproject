9283189193e373b84782a51f873e4373fff6ddbe
xianglongsdu <xianglongsdu@163.com>
2018-06-10 23:22:05 +0800

    分类、标签完成-20180610

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    re_path('post/(?P<post_id>[0-9]+)/', views.detail, name='detail'),
+    re_path('category/(?P<category_id>[0-9]+)/', views.category, name='category'),
+    re_path('archive/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/', views.archive, name='archive'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category, Tag
+import markdown
 
 def index(request):
     posts = Post.objects.all().order_by('-time')
-    return render(request, 'blog/index.html', context = {'posts': posts})
+    categories = Category.objects.all()
+    latest = Post.objects.all()[:5]
+    dates = Post.objects.dates('time', 'month', 'DESC')
+    return render(request, 'blog/index.html', context = {'posts': posts, 'categories': categories, 'latest': latest, 'dates': dates})
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.content = markdown.markdown(post.content, extensions = [
+                                            'markdown.extensions.extra',
+                                            'markdown.extensions.codehilite',
+                                            'markdown.extensions.toc'
+                                    ])
+    return render(request, 'blog/detail.html', context={'post': post})
+
+def category(request, category_id):
+    posts = Post.objects.filter(category_id = category_id)
+    categories = Category.objects.all()
+    latest = Post.objects.all()[:5]
+    dates = Post.objects.dates('time', 'month', 'DESC')
+    return render(request, 'blog/index.html', context = {'posts': posts, 'categories': categories, 'latest': latest, 'dates': dates})
+
+def archive(request, year, month):
+    posts = Post.objects.filter(time__year = year, time__month = month)
+    categories = Category.objects.all()
+    latest = Post.objects.all()[:5]
+    dates = Post.objects.dates('time', 'month', 'DESC')
+    return render(request, 'blog/index.html', context = {'posts': posts, 'categories': categories, 'latest': latest, 'dates': dates})
