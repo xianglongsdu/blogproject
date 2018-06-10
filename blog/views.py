f0d72ad8dab925f2459ec25e30d2530ba9427637
xianglongsdu <xianglongsdu@163.com>
2018-06-10 11:54:20 +0800

    Template完成-20180610“

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.all().order_by('-time')
+    return render(request, 'blog/index.html', context = {'posts': posts})
